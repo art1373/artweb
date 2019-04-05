a07354f71059d0fb9e4ad4caae145fcee3d2b6b4
alireza <alirezatk73@gmail.com>
2019-04-05 22:28:06 +0430

    fonts and src main files

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/alipic/Logo';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import Posts from './components/Posts/Posts';
+import CV from './components/CV/CV';
+//import ContactMe from './components/ContactMe/ContactMe';
+
+
+const screenWidth = window.innerWidth;
+var width = (screenWidth <= 360) ? 86 : 50;
+var perWidth = {width: width + '%'};
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      pages: <Posts />,
+      images:['./1.jpg', './2.jpg'],
+    };
+  }
+  onRouteChange = (route) =>{
+    var pages = '';
+    switch(route){
+      case 0: pages = <Posts />; break;
+      case 1: pages = <CV />; break;
+      //case 2: pages = <ContactMe />; break;
+      default: pages = <Posts />;
+    };
+    this.setState({pages: pages});
+  }
+
+  render() {
+   return (
+          <div className="App" style={perWidth}>
+            <Navigation />  
+            <Logo />
+            <Nav onRouteChange={this.onRouteChange} />
+            {this.state.pages}
+            <Footer />
+          </div>
+        ); 
+  }
+}
+
+export default App;
